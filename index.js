02da126078ac35c1561dd760be4f147d820ebfdb
Aastha <saxena.aastha91@gmail.com>
2023-07-18 16:18:16 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names,word){
+    let thankYouMessages=[]
+    for(let i=0;i<names.length;i++){
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${word} gift!`)
+
+    }
+    return thankYouMessages 
+}
+
+function countDown(n){
+    while (n>=0){
+        console.log(n)
+        n=n-1
+    } 
+}
